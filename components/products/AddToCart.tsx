ba9ae32409e04be318f039d92fac5f4e7d303b3a
aminejih <amine@armadadelivery.com>
2024-03-10 22:27:06 +0100

    add to cart

---
+++ components/products/AddToCart.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React, { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+
+import useCartService from "@/lib/hooks/useCartStore"
+import { OrderItem } from "@/lib/models/OrderModel"
+
+export default function AddToCart({ item }: {item: OrderItem}) {
+ // const router = useRouter()
+  const { items, increase } = useCartService()
+  const [existItem, setExistItem] = useState<OrderItem | undefined>()
+  
+  useEffect(() => {
+    setExistItem(items.find(x => x.slug === item.slug))
+  }, [item, items])
+
+  const addToCartHandler = () => {
+    increase(item)
+  }
+
+  return existItem ? (
+    <div>
+      <button className="btn" type="button">
+        -
+      </button>
+      <span className="px-2" >{existItem.qty}</span>
+      <button className="btn" type="button" onClick={() => increase(existItem)} >
+        +
+      </button>
+    </div>
+  ) : (
+    <button
+      className="btn btn-primary w-full"
+      type="button"
+      onClick={addToCartHandler}
+    >
+      Add to cart 
+    </button>
+  )
+}
+
---
+++ lib/data.ts
@@ -0,0 +1,48 @@
+const data = {
+  products: [
+    {
+      name        : 'Free-shirt',
+      slug        : 'free-shirt',
+      image       : '/images/shirt1.jpg',
+      banner      : '/images/banner1.jpg',
+      price       : 70,
+      brand       : 'loiseau',
+      description : 'A popular shirt',
+      category    : 'Shirts',
+      rating      : 4.5,
+      numReviews  : 8,
+      countInStock: 20,
+      isFeatured  : true
+    },
+    {
+      name        : 'Fit-shirt',
+      slug        : 'fit-shirt',
+      image       : '/images/shirt2.jpg',
+      banner      : '/images/banner2.jpg',
+      price       : 80,
+      brand       : 'loiseau',
+      description : 'A popular shirt',
+      category    : 'Shirts',
+      rating      : 3.2,
+      numReviews  : 10,
+      countInStock: 20,
+      isFeatured  : true
+    },
+    {
+      name        : 'Slim-shirt',
+      slug        : 'slim-shirt',
+      image       : '/images/shirt3.jpg',
+      banner      : '/images/banner3.jpg',
+      price       : 120,
+      brand       : 'loiseau',
+      description : 'A popular shirt',
+      category    : 'Shirts',
+      rating      : 4.5,
+      numReviews  : 25,
+      countInStock: 4,
+      isFeatured  : true
+    }
+  ]
+}
+
+export default data
